1beacaa81c029e24adad900efbb8208f6b47f113
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-06-22 18:20:04 +0700

    make livewire for each layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Beranda;
+use App\Livewire\User;
+use App\Livewire\Laporan;
+use App\Livewire\Produk;
+use App\Livewire\Transaksi;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +13,20 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// sebelum mengakses 5 url ini perlu login pake middleware
+
+// Versi rumit
+// Route::get('home', Beranda::class)->middleware(['auth'])->name('home');
+// Route::get('user', User::class)->middleware(['auth'])->name('user');
+// Route::get('laporan', Laporan::class)->middleware(['auth'])->name('laporan');
+// Route::get('produk', Produk::class)->middleware(['auth'])->name('produk');
+// Route::get('transaksi', Transaksi::class)->middleware(['auth'])->name('transaksi');
+
+// versi simpel
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', Beranda::class)->name('home');
+    Route::get('user', User::class)->name('user');
+    Route::get('laporan', Laporan::class)->name('laporan');
+    Route::get('produk', Produk::class)->name('produk');
+    Route::get('transaksi', Transaksi::class)->name('transaksi');
+});
