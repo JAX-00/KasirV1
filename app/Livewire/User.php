777003678cefd14ae9b174d9f3447a2d64b71990
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-06-23 21:13:52 +0700

    Add Buttom for CRUD

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -3,11 +3,19 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User as ModelUser;
 
 class User extends Component
 {
+    public $ChoseMenu = 'see';
+    public function choseMenu($menu)
+    {
+        $this->ChoseMenu = $menu;
+    }
     public function render()
     {
-        return view('livewire.user');
+        return view('livewire.user')->with([
+            'allusers' => ModelUser::all()
+        ]);
     }
 }
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -1,3 +1,90 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12">
+                <button wire:click="choseMenu('see')"
+                    class="btn {{ $ChoseMenu == 'see' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    All Users
+                </button>
+                <button wire:click="choseMenu('add')"
+                    class="btn {{ $ChoseMenu == 'add' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Add Users
+                </button>
+                <button wire:loading class="btn btn-info">
+                    Loading..
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                @if ($ChoseMenu == 'see')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            All Users
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th>Data</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($allusers as $user)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->role }}</td>
+                                            <td>
+                                                <button wire:click="choseMenu('edit')"
+                                                    class="btn {{ $ChoseMenu == 'edit' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    Edit Users
+                                                </button>
+                                                <button wire:click="choseMenu('delete')"
+                                                    class="btn {{ $ChoseMenu == 'delete' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    delete Users
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'add')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Add User
+                        </div>
+                        <div class="card-body">
+                            Test
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'edit')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Edit User
+                        </div>
+                        <div class="card-body">
+                            Test
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'delete')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Delete User
+                        </div>
+                        <div class="card-body">
+                            Test
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
 </div>
