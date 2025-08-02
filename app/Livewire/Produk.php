94b5d29aa029eaf64c3e985a79ba595d519e4545
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-08-02 19:30:47 +0700

    update progress prpogram

--- app/Livewire/Produk.php
+++ app/Livewire/Produk.php
@@ -2,12 +2,107 @@
 
 namespace App\Livewire;
 
+use App\Models\Product as ModelProduct;
 use Livewire\Component;
 
 class Produk extends Component
 {
+    public $ChoseMenu = 'see';
+    public $name;
+    public $email;
+    public $role;
+    public $password;
+    public $choseProduct;
+
+    public function chooseEdit($id)
+    {
+        $this->choseProduct = ModelUser::findOrFail($id);
+        $this->name = $this->choseProduct->name;
+        $this->email = $this->choseProduct->email;
+        $this->role = $this->choseProduct->role;
+        $this->ChoseMenu = 'edit';
+    }
+
+    public function update()
+    {
+        // make validation
+        $this->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email,' . $this->choseProduct->id],
+            'role' => 'required',
+        ], [
+            // Show massage if the condition not met
+            'name.required' => 'Name is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Format should be an email address',
+            'email.unique' => 'The email is already registered',
+            'role.required' => 'Role is required',
+        ]);
+        $save = $this->choseProduct;
+        $save->name = $this->name;
+        $save->email = $this->email;
+        $save->role = $this->role;
+        $save->save();
+        if ($this->password) {
+            $save->password = bcrypt($this->password);
+        }
+        $this->reset(['name', 'email', 'role', 'choseProduct']);
+        $this->ChoseMenu = 'see';
+    }
+
+    public function chooseDelete($id)
+    {
+        $this->choseProduct = ModelUser::findOrFail($id);
+        $this->ChoseMenu = 'delete';
+    }
+
+    public function delete()
+    {
+        $this->choseProduct->delete();
+        $this->reset();
+    }
+
+    public function cancel()
+    {
+        $this->reset();
+    }
+
+    public function save()
+    {
+        // make validation
+        $this->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'role' => 'required',
+            'password' => 'required'
+        ], [
+            // Show massage if the condition not met
+            'name.required' => 'Name is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Format should be an email address',
+            'email.unique' => 'The email is already registered',
+            'role.required' => 'Role is required',
+            'password.required' => 'Password is required',
+        ]);
+        $save = new ModelUser();
+        $save->name = $this->name;
+        $save->email = $this->email;
+        $save->password = bcrypt($this->role);
+        $save->role = $this->role;
+        $save->save();
+
+        $this->reset(['name', 'email', 'role', 'password']);
+        $this->ChoseMenu = 'see';
+    }
+
+
+    public function choseMenu($menu)
+    {
+        $this->ChoseMenu = $menu;
+    }
+
     public function render()
     {
-        return view('livewire.produk');
+        return view('livewire.produk')->with(['allProduct' => ModelProduct::all()]);
     }
 }
--- resources/views/livewire/produk.blade.php
+++ resources/views/livewire/produk.blade.php
@@ -1,3 +1,164 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12">
+                <button wire:click="choseMenu('see')"
+                    class="btn {{ $ChoseMenu == 'see' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    All Users
+                </button>
+                <button wire:click="choseMenu('add')"
+                    class="btn {{ $ChoseMenu == 'add' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Add Users
+                </button>
+                <button wire:loading class="btn btn-info">
+                    Loading..
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                @if ($ChoseMenu == 'see')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            All Users
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Barcode</th>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Stock</th>
+                                    <th>Data</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($allProduct as $product)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $product->barcode }}</td>
+                                            <td>{{ $product->name }}</td>
+                                            <td>{{ $product->price }}</td>
+                                            <td>{{ $product->stock }}</td>
+                                            <td>
+                                                <button wire:click="chooseEdit({{ $product->id }})"
+                                                    class="btn {{ $ChoseMenu == 'edit' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    Edit products
+                                                </button>
+                                                <button wire:click="chooseDelete({{ $product->id }})"
+                                                    class="btn {{ $ChoseMenu == 'delete' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    delete products
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'add')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Add User
+                        </div>
+                        <div class="card-body">
+                            <form action="" wire:submit='save'>
+                                <label for="">Name</label>
+                                <input type="text" class="form-control" wire:model='name' />
+                                @error('name')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Email</label>
+                                <input type="email" class="form-control" wire:model='email' />
+                                @error('email')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Password</label>
+                                <input type="password" class="form-control" wire:model='password' />
+                                @error('password')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Role</label>
+                                <select name="" id="" class="form-control" wire:model="role">
+                                    <option value="">--Choose your role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="cashier"> Casher</option>
+                                </select>
+                                @error('role')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+                                <button type="submit" class="btn btn-primary mt-4">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'edit')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Edit User
+                        </div>
+                        <div class="card-body">
+                            <form action="" wire:submit='update'>
+                                <label for="">Name</label>
+                                <input type="text" class="form-control" wire:model='name' />
+                                @error('name')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Email</label>
+                                <input type="email" class="form-control" wire:model='email' />
+                                @error('email')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Password</label>
+                                <input type="password" class="form-control" wire:model='password' />
+                                @error('password')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+
+                                <label for="">Role</label>
+                                <select name="" id="" class="form-control" wire:model="role">
+                                    <option value="">--Choose your role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="cashier"> Casher</option>
+                                </select>
+                                @error('role')
+                                    <Span class="text-danger">{{ $message }}</Span>
+                                @enderror
+                                <br />
+                                <button type="submit" class="btn btn-primary mt-4">Save</button>
+                                <button type="button" class="btn btn-secondary mt-4"
+                                    wire:click='cancel'>Cancel</button>
+                            </form>
+                        </div>
+                    </div>
+                @elseif ($ChoseMenu == 'delete')
+                    <div class="card border-danger">
+                        <div class="card-header bg-danger text-white">
+                            Delete User
+                        </div>
+                        <div class="card-body">
+                            Do you sure to delete this user ?
+                            <p>Name: {{ $choosedUser->name }}</p>
+
+                            <button class="btn btn-danger" wire:click='delete'>Delete</button>
+                            <button class="btn btn-secondary" wire:click='cancel'>Cancel</button>
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
 </div>
